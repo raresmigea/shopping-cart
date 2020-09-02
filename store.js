85058771211330701a84190ce6b9f2cece6caf2e
raresmigea <raresini@yahoo.com>
2020-09-02 17:29:46 +0300

    updated text styling

--- store.js
+++ store.js
@@ -2,19 +2,15 @@ $('#0')
   .find('.cart-quantity-input')
   .change(function () {
     var quantity = parseFloat($('#0').find('.cart-quantity-input').val());
-    console.log(
-      'valoare inputului2  ' + $('#0').find('.cart-quantity-input').val()
-    );
+
     var price = parseFloat($('#0').find('.cart-price').text().replace('€', ''));
-    console.log(price);
+
     $('#0')
       .find('.cart-subtotal')
       .text(quantity * price + '€');
   });
 
 function subTotal(element) {
-  console.log(element);
-  console.log(element.parents('.cart-row').attr('id'));
   var quantity = element.siblings('.cart-quantity-input').val();
   var price = parseFloat(
     element.closest('.cart-row').find('.cart-price').text().replace('€', '')
@@ -29,11 +25,9 @@ $('#1')
   .find('.cart-quantity-input')
   .change(function () {
     var quantity = parseFloat($('#1').find('.cart-quantity-input').val());
-    console.log(
-      'valoare inputului2  ' + $('#1').find('.cart-quantity-input').val()
-    );
+
     var price = parseFloat($('#1').find('.cart-price').text().replace('€', ''));
-    console.log(price);
+
     $('#1')
       .find('.cart-subtotal')
       .text(quantity * price + '€');
@@ -42,18 +36,14 @@ $('#2')
   .find('.cart-quantity-input')
   .change(function () {
     var quantity = parseFloat($('#2').find('.cart-quantity-input').val());
-    console.log(
-      'valoare inputului2  ' + $('#2').find('.cart-quantity-input').val()
-    );
+
     var price = parseFloat($('#2').find('.cart-price').text().replace('€', ''));
-    console.log(price);
     $('#2')
       .find('.cart-subtotal')
       .text(quantity * price + '€');
   });
 
 function total() {
-  console.log('muie');
   var subtotal0 = parseFloat(
     $('#0').find('.cart-subtotal').text().replace('€', '')
   );
@@ -72,19 +62,14 @@ function total() {
 }
 
 $('.plus').on('click', function () {
-  console.log('click');
-  // var thisId = $(this).parents('.cart-row').attr('id');
   var increment = parseInt($(this).siblings('input').val());
   increment++;
-  console.log($(this).siblings('input').val());
   $(this).siblings('input').val(increment);
   subTotal($(this));
   total();
 });
 
 $('.minus').on('click', function () {
-  console.log('click');
-  // var thisId = $(this).parents('.cart-row').attr('id');
   var decrement = parseInt($(this).siblings('input').val());
   if (decrement) {
     decrement--;
